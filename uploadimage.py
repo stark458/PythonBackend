db1e4c32b90e77bc90f16982c3aca5f2249c5a9d
ujwol <32188417+stark458@users.noreply.github.com>
2022-08-28 15:53:30 -0500

    Add files via upload

--- uploadimage.py
+++ uploadimage.py
@@ -5,8 +5,16 @@ import tornado.ioloop
 
 class basicImageRequestHandler(tornado.web.RequestHandler):
     def get(self):
-        self.render('index.html')
-    
+        self.render('index_image.html')
+    def post(self):
+        files = self.request.files["imgFile"]
+        for f in files:
+            fh = open(f'Images/{f.filename}','wb')
+            fh.write(f.body)
+            fh.close()
+        self.write(f"'http://localhost:9999/Images/{f.filename}'")
+
+
 class imageUpload(tornado.web.RequestHandler):
     def get(self):
         return
